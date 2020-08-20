d452f3cce8b90a0900aeed9086b38bf1b8082223
Alif Munim <alif.munim@ryerson.ca>
2020-08-19 23:46:11 -0400

    Set up separate test programs for nlu and stt
    Signed-off-by: Alif Munim <alif.munim@ryerson.ca>

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Pandemic Phone Line
 An automated phone line that processes food orders through speech to text and natural language understanding.
 
-### Getting Started
+### 🏁 Getting Started
 1. Clone the repo
 `git clone https://github.com/alif-munim/pandemic-phone-line.git`
 1. Install the required dependencies:
@@ -9,6 +9,16 @@ An automated phone line that processes food orders through speech to text and na
 1. Process the sample audio file:
 `node processAudio.js daniel-order.mp3`
 
-### Creating Your Own Audio File
+### 🔉 Creating Your Own Audio File
 Below is the speech format that works best. For now, you'll have to act as both the operator and customer. The ellipses [...] signify a pause in speech.
 > "Hi, please provide your full name... **[full name]**... please provide your order... **[order, with quantities]**... please provide your address... **[house number x on y street, city, province, country]**"
+
+### 💬 Testing the NLU Model
+1. Create a sample text file following the above audio file format and save it in your local repository.
+1. Run the following command and pass your text file as an argument:
+    `node nlu.js [text-file]`
+
+### 🎙 Testing Speech To Text
+1. Create a sample audio file and upload it to a downloadable url
+1. Run the following command and pass your url as an argument:
+    `node stt.js [url]`
--- nlu.js
+++ nlu.js
@@ -1,5 +1,36 @@
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
+const fs = require("fs");
+
+const rs = fs.createReadStream(process.argv[2]);
+const textArr = [];
+
+rs.on("data", (data) => {
+  textArr.push(data.toString());
+})
+rs.on("error", (error) => {
+  console.log(error);
+})
+rs.on("close", () => {
+  // console.log(textArr.join(''));
+  const analyzeParams = {
+    'text': textArr.join(''),
+    'features': {
+      'entities': {
+        'model': '30a92200-7b6f-46fe-8cee-83bf345f76d8',
+        'limit': 10
+      }
+    }
+  };
+  
+  naturalLanguageUnderstanding.analyze(analyzeParams)
+    .then(analysisResults => {
+      console.log(JSON.stringify(analysisResults, null, 2));
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
+});
 
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2019-07-12',
@@ -9,20 +40,3 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   url: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/e58783ce-347c-4533-8e3c-a005a255b1e0',
 });
 
-const analyzeParams = {
-  'text': 'Hi Please provide your full name Vijender please please give your order black gram 2 kg onions 1 kg flour 2 kg and Dettol Please provide your address house number 101 Vasant Vihar New Delhi Thanks will you and bring you home',
-  'features': {
-    'entities': {
-      'model': '30a92200-7b6f-46fe-8cee-83bf345f76d8',
-      'limit': 10
-    }
-  }
-};
-
-naturalLanguageUnderstanding.analyze(analyzeParams)
-  .then(analysisResults => {
-    console.log(JSON.stringify(analysisResults, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
-  });
