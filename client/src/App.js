5cad1dc9ae7f0bf09543f794cdc9e894f7eb313a
Alif Munim <alif.munim@ryerson.ca>
2020-08-21 23:24:34 -0400

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import "../node_modules/jquery/dist/jquery.min.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
 import Nav from "./components/Nav";
 import Home from "./components/Home";
+import Inventory from "./components/Inventory";
+import Dashboard from "./components/Dashboard";
+import Orders from "./components/Orders";
+import Archive from "./components/Archive";
 
 function App() {
 	return (
-		<div className="App">
+		<Router>
 			<Nav />
-			<Home />
-		</div>
+			<Route exact path="/" component={Home} />
+			<Route exact path="/dashboard" component={Dashboard} />
+			<Route exact path="/inventory" component={Inventory} />
+			<Route exact path="/orders" component={Orders} />
+			<Route exact path="/archive" component={Archive} />
+		</Router>
 	);
 }
 
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
 	return (
 		<Fragment>
 			<nav className="ml-auto navbar navbar-expand-lg navbar-light bg-light">
-				<a className="navbar-brand" href="#">
+				<Link to="/" className="navbar-brand">
 					SuperEats
-				</a>
+				</Link>
 				<button
 					className="navbar-toggler"
 					type="button"
@@ -22,24 +23,24 @@ function Nav() {
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav ml-auto">
 						<li className="nav-item active">
-							<a className="nav-link" href="#">
+							<Link to="/dashboard" className="nav-link">
 								Dashboard
-							</a>
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#">
+							<Link to="/inventory" className="nav-link">
 								Inventory
-							</a>
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#">
+							<Link to="/orders" className="nav-link">
 								Orders
-							</a>
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#">
+							<Link to="/archive" className="nav-link">
 								Archive
-							</a>
+							</Link>
 						</li>
 					</ul>
 				</div>
