e909b24c8fe7893eef793302873a4c500e168c7c
15679360 <syedfaraz.hussain@mail.utoronto.ca>
2020-08-23 02:17:11 -0400

    Added bar char for orders vs date

---
+++ client/src/components/DashboardComponents/OrdersDate.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  } from 'recharts';
+
+function OrdersDate() {
+	const [data, setData] = useState([{}]);
+
+	useEffect(() => {
+        axios
+			.get("http://localhost:5000/orders/")
+			.then((res) => {
+                var currData = {};
+                var sliced;
+                for (var i = 0; i < res.data.length; i++) {
+                    sliced =  res.data[i].date.slice(0, 10);
+                    if (currData[sliced] == undefined) {
+                        currData[sliced] = 1;
+                    } else {
+                        currData[sliced] += 1;
+                    }
+                }
+                var transformedData = [];
+                for (var key in currData) {
+                    transformedData.push({date: key, orders: currData[key]});
+                }
+                console.log(transformedData);
+                setData(transformedData);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+	}, []);
+
+	return (
+		<div>
+            <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                top: 5, right: 30, left: 20, bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="orders" fill="#8884d8" />
+            </BarChart>
+		</div>
+	);
+}
+
+export default OrdersDate;
