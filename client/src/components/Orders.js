bff8540f3ac8cd6d49e04b17d77fa4e83ce0e7e8
Alif Munim <alif.munim@ryerson.ca>
2020-08-22 20:31:35 -0400

    Add routing to individual order component, ProcessOrder

--- client/src/components/Orders.js
+++ client/src/components/Orders.js
@@ -1,46 +1,66 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Table, Button, Column } from "antd";
+import { Link } from "react-router-dom";
 
 function Orders() {
 	const [orders, setOrders] = useState([{}]);
-	
+
 	const columns = [
 		{
-		  title: 'Name',
-		  dataIndex: 'name',
-		  key: 'name',
+			title: "Name",
+			dataIndex: "name",
+			key: "name",
 		},
 		{
-		  title: 'Address',
-		  dataIndex: 'address',
-		  key: 'address',
-		}
-	  ];
+			title: "Address",
+			dataIndex: "address",
+			key: "address",
+		},
+	];
 
 	useEffect(() => {
-		axios.get("http://localhost:5000/orders/")
-		.then((res) => {
-			setOrders(res.data.filter(function(order) {return order.completed == false;}));
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-	  }, []);
+		axios
+			.get("http://localhost:5000/orders/")
+			.then((res) => {
+				setOrders(
+					res.data.filter(function (order) {
+						return order.completed == false;
+					})
+				);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 
 	return (
 		<div>
-			<Table dataSource={orders}>
-				<Table.Column title="Name" dataIndex="name" key="name" />
-				<Table.Column title="Address" dataIndex="address" key="address" />
-				<Table.Column
-					title="Action"
-					key="action"
-					render={() => (
-						<Button onClick={() => {process()}}>Process</Button>
-					)}
-				/>
-			</Table>
+			{orders.forEach((order) => {
+				console.log(order);
+			})}
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Address</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					{orders.map((order) => (
+						<tr>
+							<td>{order.name}</td>
+							<td>{order.address}</td>
+							<Link to={`/processOrder/${order._id}`}>
+								<button type="button" class="btn btn-primary">
+									Process
+								</button>
+							</Link>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 }
